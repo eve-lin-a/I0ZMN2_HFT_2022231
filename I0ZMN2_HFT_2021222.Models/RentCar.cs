20a0e3278f0f08226ddd1012c381e3d864b938bb
oroszevelin <oroszevelin@stud.uni-obuda.hu>
2022-10-23 17:30:37 +0200

    RentCar key created

--- I0ZMN2_HFT_2021222.Models/RentCar.cs
+++ I0ZMN2_HFT_2021222.Models/RentCar.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,17 @@ namespace I0ZMN2_HFT_2021222.Models
 {
     public class RentCar
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RentId { get; set; }
         public string BuyerName { get; set; }
         public int BuyDate { get; set; }
         public string BuyerGender { get; set; }
         public bool IsFirstCar { get; set; }
+        [NotMapped]
         public virtual Car Car { get; set; }
+        [ForeignKey(nameof(Car))]
+        public int? CarId { get; set; }
 
     }
 }
