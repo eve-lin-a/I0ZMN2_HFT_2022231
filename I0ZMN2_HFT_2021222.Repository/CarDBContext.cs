b75308af058fcf4f29450e6904847fa753c7f600
oroszevelin <oroszevelin@stud.uni-obuda.hu>
2022-10-23 17:40:24 +0200

    CarDBContext properties created

--- I0ZMN2_HFT_2021222.Repository/CarDBContext.cs
+++ I0ZMN2_HFT_2021222.Repository/CarDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using I0ZMN2_HFT_2021222.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,8 @@ namespace I0ZMN2_HFT_2021222.Repository
 {
     public class CarDBContext: DbContext
     {
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<RentCar> Rents { get; set; }
     }
 }
