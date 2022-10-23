71baa1110e694714a5c522e2ebc31a23e5dd5b13
oroszevelin <oroszevelin@stud.uni-obuda.hu>
2022-10-23 17:17:41 +0200

    RentCar class properties created

--- I0ZMN2_HFT_2021222.Models/RentCar.cs
+++ I0ZMN2_HFT_2021222.Models/RentCar.cs
@@ -8,5 +8,11 @@ namespace I0ZMN2_HFT_2021222.Models
 {
     public class RentCar
     {
+        public int RentId { get; set; }
+        public string BuyerName { get; set; }
+        public int BuyDate { get; set; }
+        public string BuyerGender { get; set; }
+        public bool IsFirstCar { get; set; }
+
     }
 }
