dda112f444b2ab433fb835e240dbd58e7ce04dd1
evelin-a <eveelliinn1@gmail.com>
2022-11-25 12:17:43 +0100

    interfaces

---
+++ I0ZMN2_HFT_2022231.Logic/IBrandLogic.cs
@@ -0,0 +1,18 @@
+﻿using I0ZMN2_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I0ZMN2_HFT_2022231.Logic
+{
+    public interface IBrandLogic
+    {
+        Brand GetBrandById(int id);
+        IEnumerable<Brand> GetAllBrand();
+        void AddNewBrand(Brand brand);
+        void UpdateBrand(Brand brand);
+        void DeleteBrand(int id);
+    }
+}
---
+++ I0ZMN2_HFT_2022231.Logic/IRentCarLogic.cs
@@ -0,0 +1,18 @@
+﻿using I0ZMN2_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I0ZMN2_HFT_2022231.Logic
+{
+    public interface IRentCarLogic
+    {
+        RentCar GetRentCarById(int id);
+        IEnumerable<RentCar> GetAllBrand();
+        void AddNewRentCar(RentCar rentcar);
+        void UpdateRentCar(RentCar rentcar);
+        void DeleteRentcar(int id);
+    }
+}
